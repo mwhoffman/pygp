4656a893a97c9cc3d82074821e2b8a322945df43
Matt Hoffman <mwh30@cam.ac.uk>
2014-04-08 12:59:23 +0100

    Add a mixin for pretty-printing models.

--- pygp/kernels/se.py
+++ pygp/kernels/se.py
@@ -12,15 +12,21 @@ import numpy as np
 
 # local imports
 from .__base import Kernel, sqdist
+from ..utils.models import Printable
 
 # exported symbols
 __all__ = ['SEARD']
 
 
-class SEARD(Kernel):
+class SEARD(Kernel, Printable):
     def __init__(self, ell, sf):
         self._logell = np.log(np.ravel(ell))
         self._logsf = np.log(sf)
+
+    def _params(self):
+        return (
+            ('ell', np.exp(self._logell)),
+            ('sf', np.exp(self._logsf)))
 
     def get(self, x1, x2=None):
         D = sqdist(np.exp(self._logell), x1, x2)
--- pygp/utils/models.py
+++ pygp/utils/models.py
@@ -12,8 +12,27 @@ import numpy as np
 import abc
 
 # exported symbols
-__all__ = ['Parameterized']
+__all__ = ['Parameterized', 'Printable']
 
 
 class Parameterized:
+    """
+    Interface for objects that are parameterized by some set of hyperparameters.
+    """
     __metaclass__ = abc.ABCMeta
+
+
+class Printable:
+    """
+    Interface for objects which can be pretty-printed as a function of their
+    hyperparameters.
+    """
+    __metaclass__ = abc.ABCMeta
+
+    @abc.abstractmethod
+    def _params(self):
+        pass
+
+    def __repr__(self):
+        substrings = ['%s=%s' % kv for kv in self._params()]
+        return self.__class__.__name__ + '(' + ', '.join(substrings) + ')'
