f859a70db12082756db9252d5aabb780e5373ce1
Matt Hoffman <mwh30@cam.ac.uk>
2014-04-11 16:44:45 +0100

    Add back the stupid reload script; deep reload still sucks.

---
+++ pygp/reload.py
@@ -0,0 +1,34 @@
+# This is a stupid workaround for crappy deep-reload behavior in python. While
+# developing I often want to reload the entire package I'm working on... so if
+# fnames contains a depth-first traversal of the modules contained in the
+# package this will import and reload all of them.
+
+fnames = """
+pygp.utils.exceptions
+pygp.utils.models
+pygp.utils
+pygp.kernels._base
+pygp.kernels._distances
+pygp.kernels.periodic
+pygp.kernels.se
+pygp.kernels
+pygp.likelihoods._base
+pygp.likelihoods.gaussian
+pygp.likelihoods
+pygp.hyper.ml
+pygp.hyper
+pygp.inference._base
+pygp.inference.exact
+pygp.inference.basic
+pygp.inference
+pygp.extra.fourier
+pygp.extra
+pygp.plotting
+pygp
+""".split()
+
+import importlib
+
+for fname in list(reversed(fnames)) + fnames:
+    module = importlib.import_module(fname)
+    reload(module)
