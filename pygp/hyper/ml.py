bd891578c982b0286355a70926d167452c93d0f6
Matt Hoffman <mwh30@cam.ac.uk>
2014-05-13 14:57:13 +0100

    Update the optimize method to take priors.
    These priors can be used for regularization (to return the MAP estimate)
    but it's mostly useful in order to fix parameters. Ie we're using this
    structure more as constraints.

--- pygp/hyper/ml.py
+++ pygp/hyper/ml.py
@@ -15,22 +15,51 @@ import scipy.optimize as so
 __all__ = ['optimize']
 
 
-def optimize(gp):
+def optimize(gp, priors=None):
     """
-    Perform type-II maximum likelihood to fit the hyperparameters of the given
-    GP object. Note that this will modify the hyperparameters of the given GP.
+    Perform type-II maximum likelihood to fit GP hyperparameters.
+
+    If given the priors object should be a dictionary mapping named parameters
+    to an object which implements `prior.nloglikelihood(hyper, grad)`. If a
+    parameter is mapped to the `None` value then this will be assumed fixed.
+
+    Note: nothing is returned by this function. Instead it will modify the
+    hyperparameters of the given GP object in place.
     """
-    def objective(hyper):
+    priors = dict() if (priors is None) else priors.copy()
+    hyper0 = gp.get_hyper()
+    active = np.ones(gp.nhyper, dtype=bool)
+    blocks = dict()
+
+    # create a dictionary which maps each parameter into its set of blocks.
+    offset = 0
+    for key, _, size in gp._params():
+        blocks[key] = slice(offset, offset+size)
+        offset += size
+
+    # loop through the priors and delete anything that corresponds to a delta
+    # prior (ie don't optimize) and remove the indices from the active set.
+    for key, prior in priors.items():
+        if prior is None:
+            active[blocks[key]] = False
+            del priors[key]
+
+    def objective(x):
+        hyper = hyper0.copy()
+        hyper[active] = x
         gp.set_hyper(hyper)
-        return gp.nloglikelihood(True)
+        nll, dnll = gp.nloglikelihood(True)
+        for key, prior in priors.items():
+            block = blocks[key]
+            p, dp = prior.nloglikelihood(hyper[block], True)
+            nll += p
+            dnll[block] += dp
+        return nll, dnll[active]
 
     # optimize the model
-    hyper, _, info = so.fmin_l_bfgs_b(objective, gp.get_hyper())
+    x, _, info = so.fmin_l_bfgs_b(objective, hyper0[active])
 
     # make sure that the gp is using the correct hypers
+    hyper = hyper0.copy()
+    hyper[active] = x
     gp.set_hyper(hyper)
-
-    # FIXME: every call to objective will update the gp object, but it's not
-    # necessarily the case that the last call to objective will be the one with
-    # the lowest objective value. we could check this however, and if
-    # gp.get_hyper() is equal to hyper then we do nothing...
