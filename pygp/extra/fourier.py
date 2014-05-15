86be248a28783ca37b429c00336a598bde8ed89b
Matt Hoffman <mwh30@cam.ac.uk>
2014-05-16 00:01:52 +0100

    Update the Fourier sample object to sample from GP priors.

--- pygp/extra/fourier.py
+++ pygp/extra/fourier.py
@@ -13,22 +13,38 @@ import scipy.linalg as sla
 
 # local imports
 from ..utils.random import rstate
+from ..inference import ExactGP
+from ..kernels._base import Kernel
+from ..utils.exceptions import ModelError
 
 # exported symbols
 __all__ = ['FourierSample']
 
 
 class FourierSample(object):
-    def __init__(self, gp, N, rng=None):
+    def __init__(self, obj, N, rng=None):
         # if given a seed or an instantiated RandomState make sure that we use
         # it here, but also within the sample_spectrum code.
         rng = rstate(rng)
 
+        if isinstance(obj, ExactGP):
+            # FIXME: for the time being this requires an ExactGP. There might be
+            # something else to do for other forms, though.
+            ndata = obj.ndata
+            kernel = obj._kernel
+            sigma = np.exp(obj._likelihood._logsigma)
+            X, y = obj._X, obj._y
+        elif isinstance(obj, Kernel):
+            ndata = 0
+            kernel = obj
+        else:
+            raise ModelError('passed object must be a Kernel or GP object')
+
         # this randomizes the feature.
-        self.W, self.alpha = gp._kernel.sample_spectrum(N, rng)
+        self.W, self.alpha = kernel.sample_spectrum(N, rng)
         self.b = rng.rand(N) * 2 * np.pi
 
-        if gp.ndata > 0:
+        if ndata > 0:
             sigma = np.exp(gp._likelihood._logsigma)
             Phi = self.phi(gp._X)
             A = np.dot(Phi.T, Phi)
