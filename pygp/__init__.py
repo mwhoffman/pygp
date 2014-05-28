826f8e124780757f1b9b30013f56f0c10e9292d9
Matt Hoffman <mwh30@cam.ac.uk>
2014-05-28 15:51:04 +0100

    Update default exports.

--- pygp/__init__.py
+++ pygp/__init__.py
@@ -1,8 +1,12 @@
-from .inference import BasicGP
-from .plotting import *
-
+# import the important sub-packages.
 from . import hyper
 from . import kernels
 from . import likelihoods
 
+# import the basic things by default
+from .inference import BasicGP
+from .hyper import optimize
+from .plotting import gpplot
+
+# and make them available.
 __all__ = ['BasicGP', 'optimize', 'gpplot']
