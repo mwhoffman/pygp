893d4622d26bbdf43ea718861e8ea4d401344266
Bobak Shahriari <bshahr@cs.ubc.ca>
2014-08-12 23:21:24 -0700

    Adding SMC meta model.

---
+++ pygp/meta/smc.py
@@ -0,0 +1,84 @@
+"""
+Meta models which take care of hyperparameter marginalization whenever data is
+added.
+"""
+
+# future imports
+from __future__ import division
+from __future__ import absolute_import
+from __future__ import print_function
+
+# global imports
+import numpy as np
+
+# local imports
+from ..learning.sampling import sample
+
+# exported symbols
+__all__ = ['SMC']
+
+
+class SMC(object):
+    def __init__(self, model, prior, n=100, burn=0):
+        self._model = model
+        self._prior = prior
+        # FIXME -- Bobak: the following should initialize according to prior
+        self._samples = [model.copy() for _ in xrange(n)]
+        self._weights = np.ones(n) / n
+        self._n = n
+        self._burn = burn   # every particle will burn before propagating
+
+        if self._model.ndata > 0:
+            self._update()
+
+        else:
+            # FIXME: the likelihood won't play a role, so we can sample directly
+            # from the prior. This of course requires the prior to also be
+            # a well-defined distribution.
+            pass
+
+    def __iter__(self):
+        return self._samples.__iter__()
+
+    def _update(self, n=1):
+        # weights are given on page 5 of (Gramacy & Polson, 2010) derived from Eq. 31
+        # in (Del Moral et al., 2006)
+        self._weights = np.array([model.loglikelihood(n) for model in self._samples])
+
+        # propagate particles according to MCMC kernel (1 step) as per Eq. 30 in
+        # (Del Moral et al., 2006)
+        self._samples = [sample(model, self._prior, 1, self._burn, raw=False)[-1]
+                         for model in self._samples]
+
+        self._model = self._samples[-1]
+
+    @property
+    def ndata(self):
+        return self._model.ndata
+
+    @property
+    def data(self):
+        return self._model.data
+
+    def add_data(self, X, y):
+        for model in self._samples:
+            model.add_data(X, y)
+        self._update(n=X.shape[0])
+
+    def posterior(self, X, grad=False):
+        parts = map(np.array, zip(*[_.posterior(X, grad) for _ in self._samples]))
+
+        mu_, s2_ = parts[:2]
+        mu = np.mean(mu_, axis=0)
+        s2 = np.mean(s2_ + (mu_ - mu)**2, axis=0)
+
+        if not grad:
+            return mu, s2
+
+        dmu_, ds2_ = parts[2:]
+        dmu = np.mean(dmu_, axis=0)
+        Dmu = dmu_ - dmu
+        ds2 = np.mean(ds2_ + 2 * mu_[   :,:,None] * Dmu
+                           - 2 * mu [None,:,None] * Dmu, axis=0)
+
+        return mu, s2, dmu, ds2
