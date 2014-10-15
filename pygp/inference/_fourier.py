71c3714fc46dfefa6f0875e1a0a8781b6aca5a8d
Matt Hoffman <mwh30@cam.ac.uk>
2014-10-16 00:54:42 +0100

    Clean up fourier samples.

--- pygp/inference/_fourier.py
+++ pygp/inference/_fourier.py
@@ -40,39 +40,35 @@ class FourierSample(object):
         self._theta = None
 
         if X is not None:
-            Phi = self.phi(X)
+            # evaluate the features
+            Z = np.dot(X, self._W.T) + self._b
+            Phi = np.cos(Z) * self._a
+
+            # get the components for regression
             A = np.dot(Phi.T, Phi) + likelihood.s2 * np.eye(Phi.shape[1])
             R = sla.cholesky(A)
             r = y - mean
+            p = np.sqrt(likelihood.s2) * rng.randn(N)
 
             # FIXME: we can do a smarter update here when the number of points
             # is less than the number of features.
 
-            rnd = np.sqrt(likelihood.s2) * rng.randn(N)
-
             self._theta = sla.cho_solve((R, False), np.dot(Phi.T, r))
-            self._theta += sla.solve_triangular(R, rnd)
+            self._theta += sla.solve_triangular(R, p)
 
         else:
             self._theta = rng.randn(N)
-
-    def phi(self, X):
-        """
-        Evaluate the random features.
-        """
-        # x is n-by-D,
-        # W is N-by-D,
-        # Phi, the return value, should be n-by-N.
-        rnd = np.dot(X, self._W.T) + self._b
-        Phi = np.cos(rnd) * self._a
-        return Phi
 
     def get(self, X):
         """
         Evaluate the function at a collection of points.
         """
-        Phi = self.phi(np.array(X, ndmin=2, copy=False))
-        return self._mean + np.dot(Phi, self._theta)
+        X = np.array(X, ndmin=2, copy=False)
+        Z = np.dot(X, self._W.T) + self._b
+        Phi = np.cos(Z) * self._a
+        F = np.dot(Phi, self._theta) + self._mean
+
+        return F
 
     def __call__(self, x):
         return self.get(x)[0]
